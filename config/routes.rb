4a96019b3dd0e7afbc490a88f0d4dbcf0daa0f02
ADI CHEO <adi.cheo@gmail.com>
2015-11-02 23:01:30 +0000

    d3 added

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ BaseApp::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   get "pages/index"
+  get 'graph/data', :defaults => { :format => 'json' }
 
   get "/admin" => "admin/base#index", :as => "admin"
 
